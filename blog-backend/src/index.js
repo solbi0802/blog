08083f54f388ac35b025b55e6c7c2dc0a5a60d90
SolbiShin <sbshin.dev@gmail.com>
2021-12-08 23:28:20 +0900

    koa 프레임워크 사용 && 라우터 설정

---
+++ blog-backend/src/index.js
@@ -0,0 +1,27 @@
+const Koa = require('koa');
+const Router = require('koa-router');
+
+const app = new Koa();
+const router = new Router();
+
+// 라우터 설정
+router.get('/', (ctx) => {
+  ctx.body = '홈';
+});
+
+router.get('/about/:name?', (ctx) => {
+  const { name } = ctx.params;
+  ctx.body = name ? `${name}의 소개` : '소개';
+});
+
+router.get('/posts', (ctx) => {
+  const { id } = ctx.query;
+  ctx.body = id ? `포스트 #${id}` : '포스트 아이디가 없습니다.';
+});
+
+// app 인스턴스에 라우터 적용
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(4000, () => {
+  console.log('Listening to port 4000');
+});
