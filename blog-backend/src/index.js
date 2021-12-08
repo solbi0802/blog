259919ce883962305dec267b6702c5fc71ac3f7b
SolbiShin <sbshin.dev@gmail.com>
2021-12-08 23:58:43 +0900

    api 만들기

--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -1,23 +1,17 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
+
+const api = require('./api');
 
 const app = new Koa();
 const router = new Router();
 
 // 라우터 설정
-router.get('/', (ctx) => {
-  ctx.body = '홈';
-});
+router.use('/api', api.routes());
 
-router.get('/about/:name?', (ctx) => {
-  const { name } = ctx.params;
-  ctx.body = name ? `${name}의 소개` : '소개';
-});
-
-router.get('/posts', (ctx) => {
-  const { id } = ctx.query;
-  ctx.body = id ? `포스트 #${id}` : '포스트 아이디가 없습니다.';
-});
+// 라우터 적용 전 bodyParser 적용
+app.use(bodyParser());
 
 // app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
