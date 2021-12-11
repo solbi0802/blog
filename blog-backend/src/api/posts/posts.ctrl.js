07491aabf61344b6e152804192c0b556592adf86
SolbiShin <sbshin.dev@gmail.com>
2021-12-11 14:46:03 +0900

    Joi를 사용해서 Request Body 검증

--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -1,5 +1,6 @@
 import Post from '../models/post';
 import mongoose from 'mongoose';
+import Joi from '@hapi/joi';
 
 const { ObjectId } = mongoose.Types;
 
@@ -22,6 +23,18 @@ export const checkObjectId = (ctx, next) => {
 */
 
 export const write = async (ctx) => {
+  const schema = Joi.object().keys({
+    title: Joi.string().required(), //필수 값
+    body: Joi.string().required(),
+    tags: Joi.array().items(Joi.string()).required(),
+  });
+  const result = schema.validate(ctx.request.body);
+  if (result.error) {
+    ctx.status = 400;
+    ctx.body = result.error;
+    return;
+  }
+
   const { title, body, tags } = ctx.request.body;
   const post = new Post({
     title,
@@ -89,6 +102,18 @@ export const remove = async (ctx) => {
 
 export const update = async (ctx) => {
   const { id } = ctx.params;
+  const schema = Joi.object().keys({
+    title: Joi.string(),
+    body: Joi.string(),
+    tags: Joi.array().items(Joi.string()),
+  });
+  const result = schema.validate(ctx.request.body);
+  if (result.error) {
+    ctx.status = 400;
+    ctx.body = result.error;
+    return;
+  }
+
   try {
     const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
       new: true, // 업데이트 된 데이터 반환
