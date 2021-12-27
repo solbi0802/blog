e3e41a16465fbe139e7c114d5b2584386e7f32ec
SolbiShin <sbshin.dev@gmail.com>
2021-12-27 22:39:31 +0900

    로그아웃 기능 구현

--- blog-frontend/src/containers/common/HeaderContainer.js
+++ blog-frontend/src/containers/common/HeaderContainer.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Header from '../../components/common/Header';
-import user from '../../modules/user';
+import { logout } from '../../lib/api/auth';
 
 const HeaderContainer = () => {
   const { user } = useSelector(({ user }) => ({ user: user.user }));
-  return <Header user={user} />;
+  const dispatch = useDispatch();
+  const onLogout = () => {
+    dispatch(logout());
+  };
+  return <Header user={user} onLogout={onLogout} />;
 };
 
 export default HeaderContainer;
